6e06e5f78f8c65d145dc9bb3331a19503adfab63
ME <597509924@qq.com>
2022-07-24 17:48:45 +0800

    first commit

---
+++ src/com/me/Hello.java
@@ -0,0 +1,11 @@
+package com.me;
+
+/**
+ * @author ME
+ * @create 2022-07-24 17:46
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+    }
+}
